99875574d543269d3e7464ae0ea81ca7dc41f971
Andrey <aau427@yandex.ru>
2026-02-27 18:19:16 +0300

    gateway сервис, 3 базы в compose.yaml. Готов к 22 спринту

--- infra/gateway/src/main/java/andrey/GateWayApp.java
+++ infra/gateway/src/main/java/andrey/GateWayApp.java
@@ -2,10 +2,12 @@ package andrey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class StoreApp {
+@EnableDiscoveryClient
+public class GateWayApp {
     public static void main(String[] args) {
-        SpringApplication.run(StoreApp.class, args);
+        SpringApplication.run(GateWayApp.class, args);
     }
 }
